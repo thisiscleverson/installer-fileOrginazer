ee32441b646d4083550c08932ce7ae86f1ad4a95
Cleverson <cleverson@pop-os.localdomain>
2022-01-08 14:23:26 -0300

    install

--- install.sh
+++ install.sh
@@ -14,10 +14,11 @@ sudo apt-get install python3 -y ##install python3
 
 sudo apt install python3-pip -y ## pip install
 
-pip3 install pyinotify
-pip3 install notify2
-pip3 install playsound
-pip3 install pyinstaller
+sudo pip3 install pyinotify
+sudo pip3 install notify2
+sudo pip3 install playsound
+sudo pip3 install pyinstaller
+
 
 sudo apt-get install git -y
 
@@ -26,14 +27,15 @@ cd /home/$USER/.local/share
 git clone https://github.com/thisiscleverson/file-organizer.git
 
 cd file-organizer
-cd dist
+pyinstaller --onefile --noconsole File_Organizer.py
+
 mv File_Organizer File_Organizer.desktop
-cp File_Organizer.desktop /home/cleversom/.config/autostart
+##cp File_Organizer.desktop /home/cleversom/.config/autostart
 
-cd /home/$USER/.local/share/file-organizer/build
-sudo cp File_Organizer.service /etc/systemd/system/
+#cd /home/$USER/.local/share/file-organizer/build
+#sudo cp File_Organizer.service /etc/systemd/system/
 
-systemd daemon-reload
+#systemd daemon-reload
 
 
 
