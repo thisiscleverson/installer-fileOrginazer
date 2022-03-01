be8780f89d01215e60ecb629b66cb1fba76b0a1c
Cleverson <cleverson@pop-os.localdomain>
2022-03-01 07:48:09 -0300

    install

--- install.sh
+++ install.sh
@@ -5,18 +5,18 @@
 
 sudo rm /var/lib/dpkg/lock-frontend ; sudo rm /var/cache/apt/archives/lock ;
 
-cd /home/
+cd /home/$USER/.local/share/
 
 sudo apt update
+sudo apt-get update 
 
 ##cd /home/$USER/.local/share
 
 sudo apt-get install git -y
 git clone https://github.com/thisiscleverson/file-organizer.git
 
-sudo apt-get update 
-sudo apt-get install python3 -y ##install python3
 
+sudo apt-get install python3 -y ##install python3
 sudo apt install python3-pip -y ## pip install
 
 sudo pip3 install pyinotify
@@ -24,14 +24,14 @@ sudo pip3 install notify2
 sudo pip3 install playsound
 sudo pip3 install pyinstaller
 
-cd /home/
-cd file-organizer
+cd /home/$USER/.local/share/file-organizer/
 
-pyinstaller --onefile --noconsole File_Organizer.py
+sudo python3 pyinstaller --onefile --noconsole File_Organizer.py
 
 ##mv File_Organizer File_Organizer.desktop
 
-cd .exec/
+cd /home/$USER/.local/share/file-organizer/.exec/
+
 cp File_Organizer.desktop /home/cleversom/.config/autostart
 
 #cd /home/$USER/.local/share/file-organizer/build
